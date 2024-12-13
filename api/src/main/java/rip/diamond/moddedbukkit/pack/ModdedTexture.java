b79f18256b177d0a04dc8ada317ef574ba14f9a6
GoodestEnglish <matthewlikemangogaming@gmail.com>
2024-12-12 23:48:23 -0500

    Added ModdedTexture javadoc

--- api/src/main/java/rip/diamond/moddedbukkit/pack/ModdedTexture.java
+++ api/src/main/java/rip/diamond/moddedbukkit/pack/ModdedTexture.java
@@ -13,12 +13,35 @@ import java.io.InputStream;
  */
 public interface ModdedTexture {
 
+    /**
+     * Get the unique key for this object.
+     * This is mostly used to identify the name for this object.
+     *
+     * @return The unique key
+     */
     Key getKey();
 
+    /**
+     * Get the texture key for this object.
+     * Texture key is used for locate where should the texture goes in the resource pack.
+     *
+     * @return The texture key
+     */
     Key getTextureKey();
 
+    /**
+     * Get the path of the texture image file, which located inside the plugin.
+     * The InputStream is used to extract the texture image file to the plugin folder.
+     *
+     * @return The InputStream
+     */
     InputStream getTextureResource();
 
+    /**
+     * Create the Creative Model class, which represent the model json file.
+     *
+     * @return The Model
+     */
     Model createModel();
 
 }
